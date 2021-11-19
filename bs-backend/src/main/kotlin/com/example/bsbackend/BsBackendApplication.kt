36d65ce3d2cd5ff2dd20772ca62ddec34ee7ff69
machnikovsky <fraczekjakub1999@gmail.com>
2021-11-19 13:07:14 +0100

    first commit

---
+++ bs-backend/src/main/kotlin/com/example/bsbackend/BsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bsbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BsBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BsBackendApplication>(*args)
+}
---
+++ bs-backend/src/test/kotlin/com/example/bsbackend/BsBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bsbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BsBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
